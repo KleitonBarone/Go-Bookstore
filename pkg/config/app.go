98c559bd12015a50dbd59f4b82d51d345637e04a
Kleiton Barone <kleitonpolar6@gmail.com>
2023-01-09 21:08:38 -0300

    Changed Docker Compose config and database config

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,8 +1,17 @@
 package config
 
 import(
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+const (
+	DB_NAME = "bookstore"
+	DB_HOST = "localhost"
+	DB_USER = "admin"
+	DB_PASS = "admin"
+	DB_PORT = "3306"
 )
 
 var (
@@ -10,7 +19,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "admin:admin@tcp(127.0.0.1:3306/bookstore?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME))
 	if err != nil {
 		panic(err)
 	}
